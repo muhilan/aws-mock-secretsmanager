a02f9fc072552ac574b20c6221cc9fc8d0a447f2
Muhilan Mahalingam Govindaraj <muhilan.mg@experian.com>
2019-03-25 20:19:25 -0700

    Tweaks

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# UNDER DEV T
+# Introduction
+
+aws-mock-secretsmanager is a lightweight AWS secretsmanager implemented in golang, mostly useful in tests.
+
+####Supported Operations
+
+get-secret-value
+
+#### Adding data
+
+Mount a folder containing certs/secrets and they will be loaded recursively
+
+##### Consider the following files in a folder
+`` ->  signing-cert.pem    ``
+
+`` ->  mysecret    ``
+ 
+`` ->  encryption.private.key    ``
+
+`` ->  encryption-cert    ``
+
+###### The following would be the Secret Ids
+``signing-cert``
+
+``mysecret``
+
+`` encryption/private ``
+
+`` encryption-cert``
+
--- main.go
+++ main.go
@@ -1,130 +1,95 @@
 package main
 
 import (
-	"io"
-	"net/http"
 	"encoding/json"
-	"log"
-	"os"
-	// "bufio"
-	"strings"
-	"path/filepath"
-	// "net/http/httputil"
-	"io/ioutil"
-	"sync"
-	"math/rand"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
 	"time"
-
 )
 
 var letterRunes = []rune("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
- var wg sync.WaitGroup
+var wg sync.WaitGroup
 
 func RandStringRunes(n int) string {
-    b := make([]rune, n)
-    for i := range b {
-        b[i] = letterRunes[rand.Intn(len(letterRunes))]
-    }
-    return string(b)
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
 }
 
 type GetSecretsResponse struct {
-	 Name string
-	 VersionId string
-	 SecretString string
-	 VersionStages []string
-	 CreatedDate int64
-	 ARN string
+	Name          string
+	VersionId     string
+	SecretString  string
+	VersionStages []string
+	CreatedDate   int64
+	ARN           string
+}
+
+type GetSecretsErrorResponse struct {
+	Type    string `json:"__type"`
+	Message string
 }
 
 type GetSecretsRequest struct {
-	 SecretId string
+	SecretId string
 }
 
 var dataMap MyMap
 
 type MyMap struct {
-	lock sync.Mutex
+	m    sync.Mutex
 	data map[string]string
 }
-
 
 func main() {
 	dataMap = MyMap{data: make(map[string]string)}
 
-	baseDir := os.Getenv("DATA_DIR")
+	baseDir, err := baseDir()
 
-	if baseDir == "" {
-		baseDir = "/data"
-	}
-	
-	if _, err := os.Stat(baseDir); os.IsNotExist(err) {
-		log.Printf("Path \"%s\" does not exist", baseDir)
+	if err != nil {
 		return
 	}
+	createMap(baseDir)
 
-	start := time.Now()
-	// Traverse filepath and update data map
-    err := filepath.Walk(baseDir, func(path string, info os.FileInfo, err error) error {
-    	if err != nil {
-			log.Printf("Error %v\n", err)
-			return err
-		}
-		// Max AWS secret size is 4 KB
-    	if !info.IsDir() && info.Size() <= 4096 {
-			go load(path, info.Name())
-			// log.Printf("Name %s, Size %d" ,info.Name(),info.Size())
-			wg.Add(1)
-		} 
-	    return nil
-    })
-
-
-    if err != nil {
-        panic(err)
-    }
-	
-	wg.Wait()
-	elapsed := time.Since(start)
-    log.Printf("Added %d secrets in %s", len(dataMap.data),elapsed)
-
-	http.HandleFunc("/", helloWorldHandler)
+	http.HandleFunc("/", postHandler)
 	http.ListenAndServe(":8080", nil)
 }
-func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+func postHandler(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method == http.MethodPost {      
+	if r.Method == http.MethodPost {
+		decoder := json.NewDecoder(r.Body)
+		var r GetSecretsRequest
+		err := decoder.Decode(&r)
+		if err != nil {
+			panic(err)
+		}
 
-	// requestDump, err := httputil.DumpRequest(r, true)
-	// if err != nil {
-	//   log.Println(err)
-	// }
-	// log.Println(string(requestDump))
+		response := GetSecretsResponse{}
+		value := dataMap.data[r.SecretId]
 
-    decoder := json.NewDecoder(r.Body)
-    var r GetSecretsRequest
-    err := decoder.Decode(&r)
-    if err != nil {
-        panic(err)
-    }
-    log.Println(r)
-	response := GetSecretsResponse{}
-	value := dataMap.data[r.SecretId]
+		if value == "" {
+			json.NewEncoder(w).Encode(GetSecretsErrorResponse{Type: "ResourceNotFoundException", Message: "Secrets Manager can’t find the specified secret."})
+			return
+		}
+		response.SecretString = value
+		response.ARN = fmt.Sprintf("arn:aws:secretsmanager:us-west-2:1234567789:secret:%s-%s", r.SecretId, RandStringRunes(6))
+		response.VersionId = RandStringRunes(6)
 
-	if value == "" {
-		log.Println("key not found " + r.SecretId)
-		io.WriteString(w, "Not found")
-		return
-	}
-	response.SecretString = value;
-	response.ARN = fmt.Sprintf("arn:aws:secretsmanager:us-west-2:1234567789:secret:%s-%s", r.SecretId, RandStringRunes(6))
-	response.VersionId = RandStringRunes(6)
-    w.Header().Add("Content-Type", "application/json")
-    w.WriteHeader(http.StatusCreated)
-    _ = json.NewEncoder(w).Encode(response)
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(response)
 
-    } else {
+	} else {
 		io.WriteString(w, "Method not supported")
 	}
 }
@@ -135,10 +100,49 @@ func load(file string, name string) {
 	if err != nil {
 		log.Println(err)
 	}
-	var key = name[0:len(name)-len(filepath.Ext(name))]
-	var finalKey = strings.Replace(key,".","/",-1)
-	dataMap.lock.Lock()
+	var key = name[0 : len(name)-len(filepath.Ext(name))]
+	var finalKey = strings.Replace(key, ".", "/", -1)
+	dataMap.m.Lock()
 	dataMap.data[finalKey] = string(content)
-	dataMap.lock.Unlock()
-	
+	dataMap.m.Unlock()
+
+}
+
+func createMap(baseDir string) {
+	start := time.Now()
+	// Traverse filepath and update data map
+	err := filepath.Walk(baseDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Printf("Error %v\n", err)
+			return err
+		}
+		// Max AWS secret size is 4 KB
+		if !info.IsDir() && info.Size() <= 4096 {
+			go load(path, info.Name())
+			wg.Add(1)
+		}
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	wg.Wait()
+	elapsed := time.Since(start)
+	log.Printf("Added %d secrets in %s", len(dataMap.data), elapsed)
+}
+
+func baseDir() (string, error) {
+	baseDir := os.Getenv("DATA_DIR")
+
+	if baseDir == "" {
+		baseDir = "/data"
+	}
+
+	if _, err := os.Stat(baseDir); os.IsNotExist(err) {
+		log.Printf("Path \"%s\" does not exist", baseDir)
+		return "", err
+	}
+	return baseDir, nil
 }
